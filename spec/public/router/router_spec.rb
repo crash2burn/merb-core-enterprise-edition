2348ee7262e8b97f11ac66971361c28c9b4e6219
Carl Lerche <carllerche@mac.com>
2008-08-31 23:52:48 -0700

    Some cleanup in the router spec file

--- spec/public/router/router_spec.rb
+++ spec/public/router/router_spec.rb
@@ -141,28 +141,13 @@ describe Merb::Router do
     end
     
   end
-  
-  describe "#route_for" do
-    
-    it "should be awesome"
-    
-  end
 
   describe "#match" do
     
     it "should raise an error if the routes were not compiled yet" do
       lambda { Merb::Router.match(simple_request) }.should raise_error(Merb::Router::NotCompiledError)
     end
-    
-    it "should choose the correct route when multiple routes are defined" do
-       Merb::Router.prepare  do |r| 
-         r.match("/denver").to(:controller => "one")
-         r.match("/houston").to(:controller => "two")
-       end
-       
-       route_to("/denver").should have_route(:controller => "one")
-       route_to("/houston").should_not have_route(:controller => "one")
-    end
+
   end
 
 end
